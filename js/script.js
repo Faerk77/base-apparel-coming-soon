b61481cb6b8d1ea5a7eeadca3fd4f86859de0364
Faerk77 <ferchuariel@gmail.com>
2022-06-19 01:24:32 -0300

    js-done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const formBtn = document.getElementById('form-btn')
 const emailInput = document.getElementById('email')
 
 formBtn.addEventListener("click", function(event){
-    let emailText = emailInput.value
     const regex = /^\w+(\@([a-z]+)(\.com)$)|^\w+(\@([a-z]+)(\.com)(\.ar)$)/g
+    let emailText = emailInput.value
     if(emailText === ""){
          event.preventDefault()
          formBtn.classList.remove('form__btn--toggle')
